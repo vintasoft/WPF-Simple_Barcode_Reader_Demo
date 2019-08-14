824c924869364471439eeda6783bee8fab5f7e02
vintasoft <D:\The Bat!>
2019-08-14 12:01:38 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.0.0.1")]
-[assembly: AssemblyFileVersion("12.0.0.1")]
+[assembly: AssemblyVersion("12.0.7.1")]
+[assembly: AssemblyFileVersion("12.0.7.1")]
 
