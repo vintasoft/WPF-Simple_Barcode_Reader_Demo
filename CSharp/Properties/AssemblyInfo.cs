b61471337380a932ae0fe08ca3ca22fc72742fbf
vintasoft <D:\The Bat!>
2022-05-11 11:50:19 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.5.4.1")]
-[assembly: AssemblyFileVersion("12.5.4.1")]
+[assembly: AssemblyVersion("12.5.4.3")]
+[assembly: AssemblyFileVersion("12.5.4.3")]
 
