13c1ac19a98dc15bfe101f72b8985ee1fbbc9252
vintasoft <alex@vintasoft.com>
2024-10-16 15:39:09 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("VintaSoft WpfSimpleBarcodeReaderDemo")]
 [assembly: AssemblyDescription("VintaSoft WPF simple barcode reader demo")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VintaSoft Ltd.")]
+[assembly: AssemblyCompany("VintaSoft Llc.")]
 [assembly: AssemblyProduct("VintaSoft Barcode .NET SDK")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2024")]
+[assembly: AssemblyCopyright("Copyright VintaSoft Llc. 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("15.0.9.1")]
-[assembly: AssemblyFileVersion("15.0.9.1")]
+[assembly: AssemblyVersion("15.0.10.1")]
+[assembly: AssemblyFileVersion("15.0.10.1")]
 
